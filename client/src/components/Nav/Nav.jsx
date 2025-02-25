1da70fa02f606987cccaf7558317e6623624fd96
Kyle-the-coder <kylethacoder@gmail.com>
2025-02-25 11:05:35 -0700

    updated

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,12 +1,21 @@
 import "./nav.css";
 import logo from "../../assets/logo/igniteLogo.png";
 export function Nav() {
+  const links = [
+    { linkName: "Home", link: "/" },
+    { linkName: "About", link: "/about" },
+    { linkName: "Projects", link: "/projects" },
+  ];
   return (
     <nav className="nav-main-container">
       <div className="logo silver-bg">
         <img src={logo} />
       </div>
-      <div className="links"></div>
+      <div className="links">
+        {links.map((link) => (
+          <h1 key={link.linkName}>{link.linkName}</h1>
+        ))}
+      </div>
       <div className="socials"></div>
     </nav>
   );
