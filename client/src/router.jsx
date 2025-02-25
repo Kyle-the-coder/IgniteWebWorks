7aab6e56a4cfffa1d42696c3bb128782e804f20f
Kyle-the-coder <kylethacoder@gmail.com>
2025-02-25 10:20:58 -0700

    updated

---
+++ client/src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import { NavLayout } from "./layout/NavLayout";
+import { ErrorLayout } from "./layout/ErrorLayout";
+// import { landingRoute } from "./pages/Landing/Landing";
+// import { aboutRoute } from "./pages/About/About";
+// import { projectPageRoute } from "./pages/ProjectPage/ProjectPage";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavLayout />,
+    errorElement: <ErrorLayout />,
+    children: [
+      //   { path: "/", ...landingRoute },
+      //   { path: "/about", ...aboutRoute },
+      //   { path: "/projects", ...projectPageRoute },
+    ],
+  },
+]);
