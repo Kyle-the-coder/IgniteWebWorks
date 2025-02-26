f875d137fd7e6c5d1de699258df025fcfd69b667
Kyle-the-coder <kylethacoder@gmail.com>
2025-02-26 10:30:03 -0700

    updated

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import heroBg from "../../assets/igniteHero.webp";
+import heroBgMobile from "../../assets/igniteHeroMobile2.jpg";
 import "./hero.css";
+
 export function Hero() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="hero-main-container">
-      <img src={heroBg} />
-      <h1 className="oswald-font white-text">
-        <span className="orange-text">IGNITE</span> Your
-      </h1>
-      <h1 className="oswald-font white-text">Online Presence</h1>
+      {windowWidth <= 600 ? (
+        <>
+          <img src={heroBgMobile} />
+          <h1 className="oswald-font white-text">
+            <span className="orange-text">IGNITE</span> Your Online Presence
+          </h1>
+        </>
+      ) : (
+        <>
+          <img src={heroBg} />{" "}
+          <h1 className="oswald-font white-text">
+            <span className="orange-text">IGNITE</span> Your
+          </h1>
+          <h1 className="oswald-font white-text">Online Presence</h1>
+        </>
+      )}
     </div>
   );
 }
