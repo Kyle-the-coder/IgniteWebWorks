6bfeb3e9f500b3815870632ba6e55cc5ad0dbf3b
Kyle-the-coder <kylethacoder@gmail.com>
2025-03-04 13:05:01 -0700

    updated

--- client/src/sections/Projects/Projects.jsx
+++ client/src/sections/Projects/Projects.jsx
@@ -8,7 +8,19 @@ import poke from "../../assets/projects/Poke.png";
 import rjp from "../../assets/projects/RJP.png";
 import rms from "../../assets/projects/RMS.png";
 import { Button } from "../../components/Button/Button";
+import { useState } from "react";
 export function Projects() {
+  const [isHover, setIsHover] = useState(false);
+  const [isHoverIndex, setIsHoverIndex] = useState(null);
+
+  function handleHoverEnter(index) {
+    setIsHoverIndex(index);
+    setIsHover(true);
+  }
+  function handleHoverLeave(index) {
+    setIsHoverIndex(null);
+    setIsHover(false);
+  }
   return (
     <section className="projects-main-container">
       <img src={waveGrad} className="waves-grad" />
@@ -25,16 +37,48 @@ export function Projects() {
         </div>
 
         <div className="projects-grid-container">
-          <div className="project-img-container">
+          <div
+            className="project-img-container"
+            onMouseEnter={() => handleHoverEnter(1)}
+            onMouseLeave={() => handleHoverLeave()}
+          >
+            <div
+              className="dark-bg"
+              style={{ opacity: isHoverIndex === 1 ? 1 : 0 }}
+            ></div>
             <img src={apc} />
           </div>
-          <div className="project-img-container">
+          <div
+            className="project-img-container"
+            onMouseEnter={() => handleHoverEnter(2)}
+            onMouseLeave={() => handleHoverLeave()}
+          >
+            <div
+              className="dark-bg"
+              style={{ opacity: isHoverIndex === 2 ? 1 : 0 }}
+            ></div>
             <img src={poke} />
           </div>
-          <div className="project-img-container">
+          <div
+            className="project-img-container"
+            onMouseEnter={() => handleHoverEnter(3)}
+            onMouseLeave={() => handleHoverLeave()}
+          >
+            <div
+              className="dark-bg"
+              style={{ opacity: isHoverIndex === 3 ? 1 : 0 }}
+            ></div>
             <img src={rms} />
           </div>
-          <div className="project-img-container">
+          <div
+            className="project-img-container"
+            onMouseEnter={() => handleHoverEnter(4)}
+            onMouseLeave={() => handleHoverLeave()}
+          >
+            <div
+              className="dark-bg"
+              style={{ opacity: isHoverIndex === 4 ? 1 : 0 }}
+            ></div>
             <img src={rjp} />
           </div>
         </div>
