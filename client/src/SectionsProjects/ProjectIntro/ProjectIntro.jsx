1e1e9882707925dcf723aaacfeaff50e2c5b3142
Kyle-the-coder <kylethacoder@gmail.com>
2025-03-26 09:08:32 -0600

    updated

---
+++ client/src/SectionsProjects/ProjectIntro/ProjectIntro.jsx
@@ -0,0 +1,17 @@
+import { ImgGrid } from "../../components/ImgGrid/ImgGrid";
+import bg from "../../assets/titlebg.jpg";
+import "./projectintro.css";
+export function ProjectIntro() {
+  return (
+    <>
+      <div className="project-display-top orange-bg">
+        <h1 className="oswald-font project-text charcoal-text">Web Projects</h1>
+      </div>
+      <div className="project-img-display-container orange-bg">
+        <img src={bg} className="project-bg" />
+        <h1 className="oswald-font white-text">Web Projects</h1>
+        <ImgGrid />
+      </div>
+    </>
+  );
+}
--- client/src/pages/ProjectsPage/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -2,18 +2,12 @@ import { ImgGrid } from "../../components/ImgGrid/ImgGrid";
 import bg from "../../assets/titlebg.jpg";
 import rjp from "../../assets/rjp.png";
 import "./projectspage.css";
+import { ProjectIntro } from "../../SectionsProjects/ProjectIntro/ProjectIntro";
 
 function ProjectsPage() {
   return (
     <div className="main-page">
-      <div className="project-display-top orange-bg">
-        <h1 className="oswald-font project-text charcoal-text">Web Projects</h1>
-      </div>
-      <div className="project-img-display-container orange-bg">
-        <img src={bg} className="project-bg" />
-        <h1 className="oswald-font white-text">Web Projects</h1>
-        <ImgGrid />
-      </div>
+      <ProjectIntro />
 
       <div className="main-page silver-bg">
         <div className="project-display">
