cf981a6b140e9124e246e7f8ff923bd6112e0be2
Kyle-the-coder <kylethacoder@gmail.com>
2025-04-10 10:23:09 -0600

    updated

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/workouts");
+const cors = require("cors");
+
+const app = express();
+const workoutRoutes = require("./routes/workouts");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+//Connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
